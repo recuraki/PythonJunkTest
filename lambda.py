116c2c42fba21cfcfc3588980b4d3b1356a0317f
Akira KANAI <glenda.kanai@gmail.com>
2013-10-23 18:27:20 +0900

    lambdaを追加。

---
+++ lambda.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+lambda式関係を色々書きためる
+"""
+
+"""
+>>> compare = lambda a,b:1 if a > b else 0 if a == b else -1
+>>> compare(1,2)
+-1
+>>> compare(2,1)
+1
+>>> compare(1,1)
+0
+"""
+compare = lambda a,b:1 if a > b else 0 if a == b else -1
+print compare(1,2)
+
+"""
+>>> GetReverseString = lambda x: ''.join(list(reversed(x)))
+>>> GetReverseString("hoge")
+'egoh'
+>>> "hoge"[::-1]
+'egoh'
+"""
+GetReverseString = lambda x: ''.join(list(reversed(x)))
+print GetReverseString("hoge")
+print "hoge"[::-1]
+
+
+"""
+>>> str2indexlist = lambda stInput: map(lambda x,y: [x,y], stInput, range(len(stInput)))
+>>> str2indexlist("hoge")
+[['h', 0], ['o', 1], ['g', 2], ['e', 3]]
+"""
+str2indexlist = lambda stInput: map(lambda x,y: [x,y], stInput, range(len(stInput)))
+print str2indexlist("hoge")
+
+"""
+>>> str2indexlist = lambda stInput: [[y, x] for x, y in enumerate(stInput)]
+>>> str2indexlist("hoge")
+[['h', 0], ['o', 1], ['g', 2], ['e', 3]]
+"""
+str2indexlist = lambda stInput: [[y, x] for x, y in enumerate(stInput)]
+print str2indexlist("hoge") 
