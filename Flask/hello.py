e1828fb128b6d81f8ee18c0a947813515f4d26aa
kanai <kanai@wide.ad.jp>
2014-01-02 15:29:20 +0900

    flaskのサンプルを追加

---
+++ Flask/hello.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return('index', 200)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    print(request.args)
+    name = request.args.get("name", "")
+    print("<br>\n")
+    c = ""
+    c += "<hr>"
+    c += "hello" + name
+    c += "<hr>"
+    return(c, 200)
+
+@app.route('/hello/<name>', methods=['GET'])
+def hello_withname(name):
+    return("hi! {0}".format(name), 200)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
