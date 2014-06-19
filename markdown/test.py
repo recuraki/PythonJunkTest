7c0ddc1c81e20ff3f1ce2b0c8088552771b76c4d
Akira KANAI <kanai@sfc.wide.ad.jp>
2014-06-19 09:48:27 +0900

    markdown追加

---
+++ markdown/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# pip install pygments
+# git clone https://github.com/dart-lang/py-gfm.git
+# cd py-gfm
+# python setup.py install
+import markdown
+
+templ = u"""
+hogehoge
+
+```
+import sys
+
+def hoge(fuga):
+  fugafuga = 123
+  print fugafuga
+  return 'ok'
+```
+
+`ほげ`です
+
+> 引用
+>> もっと引用
+"""
+
+if '__main__' == __name__:
+    codehilite = 'codehilite(force_linenos=True, guess_lang=False, css_class=syntax)'
+    html = markdown.markdown(templ, extentions = ['extra', codehilite, 'gfm'])
+    print(html.encode('utf-8'))
