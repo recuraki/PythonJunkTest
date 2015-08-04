2ef9488d53cb8d5da6e4dc3f25eafe1c41d2b541
Akira KANAI <kanai@wide.ad.jp>
2015-08-04 13:52:52 +0900

    sync

--- strings/q1.py
+++ strings/q1.py
@@ -11,7 +11,7 @@ def check_result(res):
 
 # Try1: "pen"が含まれているか確認し、含まれていたらokと表示しましょう
 print("Try1")
-query = "pen"
+query = ""
 res = re.search(query, "this is a pen.")
 check_result(res)
 
@@ -22,14 +22,14 @@ check_result(res)
 # そして、その文字列を表示しましょう。
 # ただし、ここではxは「なにかの文字」です
 print("Try2")
-query = r"a.c."
+query = ""
 res = re.search(query, "abcABC")
 check_result(res)
 
 # Try3: "a"からはじまって"d"で終わる文字列を探してください
 # 対象は"a12b23c34d45e56f"です
 print("Try3")
-query = "a.*d"
+query = ""
 res = re.search(query, "a12b23c34d45e56f")
 check_result(res)
 res = re.search(query, "ad1234")
@@ -37,7 +37,7 @@ check_result(res)
 
 # Try4: "ab"のあとに"c"が一個以上あり、"d"で終わる文字列を探してください
 print("Try4")
-query = "abc+d"
+query = ""
 res = re.search(query, "abcccdef")
 check_result(res)
 res = re.search(query, "abd")
@@ -45,7 +45,7 @@ check_result(res)
 
 # Try5: "hoge12345foo"から12345を探すように「数字の列」を探してください
 print("Try5")
-query = "[0-9]+"
+query = ""
 res = re.search(query, "hoge12345foo")
 check_result(res)
 res = re.search(query, "this is 34567")
@@ -53,7 +53,7 @@ check_result(res)
 
 # try6: fooかbarが含まれているか確認してください
 print("Try6")
-query = "(foo|bar)"
+query = ""
 res = re.search(query, "hoge12345foo")
 check_result(res)
 res = re.search(query, "this is 34567")
@@ -61,7 +61,7 @@ check_result(res)
 
 # try7: "Incounter: 数字"という文字列かどうか確認してください
 print("Try7")
-query = "Incounter: \d+"
+query = ""
 res = re.search(query, "Incounter: 100")
 check_result(res)
 res = re.search(query, "incounter: 100")
@@ -69,7 +69,7 @@ check_result(res)
 
 # try7-2: 上記の例で、「Incounter」でも「incounter」でもいいようにしてください 
 print("Try7-2")
-query = r"[Ii]ncounter: \d+"
+query = r""
 re.search(query, "Incounter: 100")
 check_result(res)
 re.search(query, "incounter: 100")
@@ -77,7 +77,7 @@ check_result(res)
 
 # try8: "incounter: 数字"から数字の部分を抜き出してください
 print("Try8")
-query = "incounter: (\d+)"
+query = ""
 
 res = re.match(query, "incounter: 100")
 # 以下の
@@ -87,7 +87,7 @@ print(res.group(1))
 
 
 # try8-2: "incounter: 数字, outcounter: 数字"から数字を抜き出してください
-print("Try8-2")
+print("")
 query = "incounter: (\d+), outcounter: (\d+)"
 res = re.match(query, "incounter: 100, outcounter: 200")
 if res is not None:
@@ -97,19 +97,19 @@ if res is not None:
 
 # try9: "foo"を"bar"に置換してください
 print("Try9")
-query = "foo"
-replaceas = "bar"
+query = ""
+replaceas = ""
 print(re.sub(query, replaceas, "I'm foo."))
 
 # try10: "incounter: 数字"を"in = 数字"に変換してください
 print("Try10")
-query = "incounter: (\d+)"
-replaceas = "in = \\1"
+query = ""
+replaceas = ""
 print(re.sub(query, replaceas, "incounter: 100"))
 
 # try10-2: "incounter: 数字, outcounter: 数字"を"in,out = 数字,数字"に変換してください
 print("Try10-2")
-query = "incounter: (\d+), outcounter: (\d+)"
-replaceas = "in,out = \\1,\\2"
+query = ""
+replaceas = ""
 print(re.sub(query, replaceas, "incounter: 100, outcounter: 200"))
 
---
+++ strings/q3_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+
+# 結果として想定される文字列をstrに代入
+str1="""
+Router>show int
+FastEthernet0 is up, line protocol is up 
+  MTU 1500 bytes, BW 100000 Kbit/sec, DLY 100 usec, 
+     reliability 255/255, txload 1/255, rxload 1/255
+  5 minute input rate 1000 bits/sec, 1 packets/sec
+  5 minute output rate 1000 bits/sec, 1 packets/sec
+FastEthernet1 is up, line protocol is up 
+  5 minute input rate 1000 bits/sec, 1 packets/sec
+  5 minute output rate 1000 bits/sec, 1 packets/sec
+
+"""
+
+str2="""
+Router>show int status 
+Port    Name               Status       Vlan       Duplex Speed Type
+Fa2     uplink             notconnect   1            auto    auto 10/100BaseTX
+Fa3     user1              notconnect   2            auto    auto 10/100BaseTX
+Fa4     server             notconnect   3            full     100 10/100BaseTX
+Fa5     none               notconnect   1            auto    auto 10/100BaseTX
+"""
+
