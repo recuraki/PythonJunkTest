64223e86ef95f17a384f9534165572529021965c
Akira KANAI <kanai@wide.ad.jp>
2016-01-29 16:25:57 +0900

    sync

--- Flask/json_as.py
+++ Flask/json_as.py
@@ -7,53 +7,61 @@ import json
 import telnetlib
 
 """
-テストは以下のように実施します。
-正常な例:
-curl -H "Content-Type:application/json" -X POST -d '{"val1": 1, "val2": 2}' http://localhost:5000/add
 """
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
+    # slackからの入力をおけとります
     stText = request.form["text"]
+
+    # 入力をそのまま関数に渡します
     res = proc_add(stText)
+
+    # 結果を返します
     c = ""
     c += str(res)
     print c
     return(c)
 
 def proc_add(stText):
+    # " "でセパレート
     liText = stText.split(" ")
-    
+
+    # 引数の数が違ったらエラーを返します
     if len(liText) != 2:
         diRet = {}
-        diRet['text'] = "oops"
+        diRet['text'] = "usage: asnum <ASNUM>"
         diRet['username'] = "asbot"
         return(json.dumps(diRet, indent = 2))
 
 
+    # 引数が整数かを確認します
     try:
         asnum = int(liText[1])
     except ValueError:
+        # 整数じゃなかったら怒ります
         diRet = {}
         diRet['text'] = "plz input int :p"
         diRet['username'] = "asbot"
         return(json.dumps(diRet, indent = 2))
 
-    print "try" + liText[1]
-    print "hoge"
+    # 返信を作ります
     diRet = {}
-    print "st"
+    # ここでaslookupする
     diRet['text'] = aslook(liText[1])
-    print "end"
     diRet['username'] = "asbot"
     return(json.dumps(diRet, indent = 2))
 
 def aslook(stNum):
+    # telnetしてほげほげ
     tn = telnetlib.Telnet(host = "asn.cymru.com", port = 43)
+    # unicodeになっているんでstrにして連結
     stQue = "AS" + str(stNum) + "\n"
+    # "AS290"とかを送信
     tn.write(stQue)
+    # 返信を全部格納
     ret = tn.read_all()
     tn.close()
     return(ret)
