b6871c3228a238a95137a04c0d426baab505bd53
Akira Kanai <kanai@wide.ad.jp>
2016-02-16 23:46:57 +0900

    s

--- RabbitMQ/1_producer.py
+++ RabbitMQ/1_producer.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 import pika
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
 channel = connection.channel()
-
 channel.queue_declare(queue='hello')
 
+msg = dict()
+msg["cmd"] = "SRXsCreate"
+msg["orderid"] = 1
+msg["cid"] = 1
+msg["wfid"] = 1
+msg["stepid"] = 1
+
+
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello World!')
+                      routing_key='SJJOB',
+                      body=json.dumps(msg))
 print(" [x] Sent 'Hello World!'")
 connection.close()
