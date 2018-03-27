92e74e3d54f1ae26e058f570bc9d90394a9aa740
Akira KANAI <kanai@wide.ad.jp>
2018-03-27 12:53:08 +0900

    sync

---
+++ strings/multi.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 結果として想定される文字列をstrに代入
+str="""
+Cisco#show standby brief 
+Load for five secs: 2%/0%; one minute: 2%; five minutes: 2%
+Interface   Grp  Pri P State   Active          Standby         Virtual IP
+Gi0/0/3.11 1    110 P Init    unknown         unknown         10.0.81.1
+Gi0/0/3.201
+            1    110 P Init    unknown         unknown         10.0.1.1
+Gi0/0/3.202
+            1    110 P Init    unknown         unknown         10.0.2.1
+Gi0/0/3.203
+            1    110 P Init    unknown         unknown         10.0.3.1
+Gi0/0/3.204
+            1    110 P Init    unknown         unknown         10.0.4.1
+Gi0/0/3.205
+            1    110 P Init    unknown         unknown         10.0.5.1
+Gi0/0/3.206
+            1    110 P Init    unknown         unknown         10.0.6.1
+Gi0/0/3.207
+            1    110 P Init    unknown         unknown         10.0.7.1
+Gi0/0/3.208
+            1    110 P Init    unknown         unknown         10.0.8.1
+Gi0/0/3.209
+            1    110 P Init    unknown         unknown         10.0.9.1
+Gi0/0/3.210
+            1    110 P Init    unknown         unknown         10.0.10.1
+Gi0/0/4     1    110 P Init    unknown         unknown         10.0.11.1
+Gi0/0/5     1    110 P Init    unknown         unknown         10.0.21.1
+"""
+
+# まず、元の文字列を表示
+print("Original:")
+print("[" + str + "]")
+
+import re
+
+# 以下のような文字列を検索し、イテレータ(for可能)にする
+# interface port <num>/<num>
+# (空白)ほにゃらら
+# (空白)ほにゃらら
+# switchport trunk add <num>(改行)
+r = re.finditer("(Gi[^ ]+)[ \n]+ +[0-9]+ +[0-9]+ +. ([^ ]+) +[^ ]+", str)
+for res in r:
+    eth = res.group(1).strip()
+    stat = res.group(2)
+    print eth,stat
