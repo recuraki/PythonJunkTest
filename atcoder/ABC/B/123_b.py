f6162b4a43f2d0ac58f7d5bdf980ac72cfe16467
Akira Kanai <kanai@wide.ad.jp>
2019-05-17 13:18:35 +0900

    ABC 123_b

---
+++ atcoder/ABC/B/123_b.py
@@ -0,0 +1,63 @@
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """29
+20
+7
+35
+120"""
+        output = """215"""
+        self.assertIO(input, output)
+    def test_入力例_2(self):
+        input = """101
+86
+119
+108
+57"""
+        output = """481"""
+        self.assertIO(input, output)
+    def test_入力例_3(self):
+        input = """123
+123
+123
+123
+123"""
+        output = """643"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+def resolve():
+    import math
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    e = int(input())
+    a = [a,b,c,d,e]
+    totaltime = 0
+    longesttime = 100
+    for i in range(5):
+        tmp = a[i] % 10
+        if tmp == 0:
+            tmp = 10
+        if tmp < longesttime:
+            longesttime = a[i] % 10
+    for i in range(5):
+        totaltime += math.ceil(a[i] / 10) * 10
+    print(totaltime - (10 - longesttime))
+
+
