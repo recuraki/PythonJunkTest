ff67228a959578ccd49d5a9af095d168cb95e3e0
Akira Kanai <kanai@wide.ad.jp>
2019-05-17 12:49:19 +0900

    ABC 124_b, 125_b

--- atcoder/ABC/B/124_b.py
+++ atcoder/ABC/B/124_b.py
@@ -12,35 +12,32 @@ class TestClass(unittest.TestCase):
         sys.stdout, sys.stdin = stdout, stdin
         self.assertEqual(out, output)
     def test_入力例_1(self):
-        input = """3
-10 2 5
-6 3 4"""
-        output = """5"""
+        input = """4
+6 5 6 8"""
+        output = """3"""
         self.assertIO(input, output)
     def test_入力例_2(self):
-        input = """4
-13 21 6 19
-11 30 6 15"""
-        output = """6"""
+        input = """5
+4 5 3 5 4"""
+        output = """3"""
         self.assertIO(input, output)
     def test_入力例_3(self):
-        input = """1
-1
-50"""
-        output = """0"""
+        input = """5
+9 5 6 8 4"""
+        output = """1"""
         self.assertIO(input, output)
 
 if __name__ == "__main__":
     unittest.main()
 
 def resolve():
-    val = 0
     n = int(input())
-    v = map(int, input().split())
-    c = map(int, input().split())
-    v = list(v)
-    c = list(c)
+    h = map(int, input().split())
+    h = list(h)
+    mh = 0
+    count = 0
     for i in range(n):
-        if c[i] < v[i]:
-            val = val + (v[i] - c[i])
-    print(val)
+        if mh <= h[i]:
+            mh = h[i]
+            count += 1
+    print(count)
