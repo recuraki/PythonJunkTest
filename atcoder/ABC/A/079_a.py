d26045c1169076b6d926b03d3a49ab9b87dd9691
Akira Kanai <kanai@wide.ad.jp>
2019-05-17 11:32:40 +0900

    ABC 079_a

---
+++ atcoder/ABC/A/079_a.py
@@ -0,0 +1,37 @@
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """1118"""
+        output = """Yes"""
+        self.assertIO(input, output)
+    def test_入力例_2(self):
+        input = """7777"""
+        output = """Yes"""
+        self.assertIO(input, output)
+    def test_入力例_3(self):
+        input = """1234"""
+        output = """No"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
+
+def resolve():
+    n = input()
+    if n[0] == n[1] and n[1] == n[2]:
+        print("Yes")
+    elif n[2] == n[3] and n[1] == n[2]:
+        print("Yes")
+    else:
+        print("No")
