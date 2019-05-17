92106b09694677c5881380ad72becb91a44847d7
Akira Kanai <kanai@wide.ad.jp>
2019-05-17 12:06:39 +0900

    ABC 063_a

---
+++ atcoder/ABC/A/063_a.py
@@ -0,0 +1,31 @@
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """6 3"""
+        output = """9"""
+        self.assertIO(input, output)
+    def test_入力例_2(self):
+        input = """6 4"""
+        output = """error"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
+
+def resolve():
+    a,b = map(int, input().split())
+    if (a+b) > 9:
+        print("error")
+    else:
+        print(a+b)
