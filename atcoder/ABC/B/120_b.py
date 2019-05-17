84e65445b6841d09e98ff7e665448eb73f470a64
Akira Kanai <kanai@wide.ad.jp>
2019-05-17 13:29:16 +0900

    ABC 120_b

---
+++ atcoder/ABC/B/120_b.py
@@ -0,0 +1,36 @@
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """8 12 2"""
+        output = """2"""
+        self.assertIO(input, output)
+    def test_入力例_2(self):
+        input = """100 50 4"""
+        output = """5"""
+        self.assertIO(input, output)
+    def test_入力例_3(self):
+        input = """1 1 1"""
+        output = """1"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
+
+def resolve():
+    a, b, k = map(int, input().split())
+    res = []
+    for i in range(1, 100):
+        if ((a % i) == 0) and ((b % i) == 0):
+            res.append(i)
+    print(res[-k])
