bdf979057ab018fb15c4274e226914d5ba1266c7
Akira Kanai <kanai@wide.ad.jp>
2019-05-25 12:41:56 +0900

    ABC

--- atcoder/ABC/B/123_b.py
+++ atcoder/ABC/B/123_b.py
@@ -2,6 +2,35 @@ import sys
 from io import StringIO
 import unittest
 
+def resolve():
+    import math
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    e = int(input())
+    dat = [a,b,c,d,e]
+    dattime = map
+    tindex = 0
+    tnum = 10
+
+    for i in range(5):
+        t = dat[i] % 10
+        t = 10 if t == 0 else t
+        if tnum > t:
+            tindex = i
+            tnum = t
+
+    sum = 0
+
+    for i in range(5):
+        if i != tindex:
+            sum += int( math.ceil(dat[i] / 10) * 10 )
+        else:
+            sum += dat[i]
+    print(sum)
+
+
 class TestClass(unittest.TestCase):
     def assertIO(self, input, output):
         stdout, stdin = sys.stdout, sys.stdin
@@ -36,28 +65,17 @@ class TestClass(unittest.TestCase):
         output = """643"""
         self.assertIO(input, output)
 
+    def test_入力例_4(self):
+        input = """10
+10
+10
+10
+10"""
+        output = """50"""
+        self.assertIO(input, output)
+
 if __name__ == "__main__":
     unittest.main()
 
-
-def resolve():
-    import math
-    a = int(input())
-    b = int(input())
-    c = int(input())
-    d = int(input())
-    e = int(input())
-    a = [a,b,c,d,e]
-    totaltime = 0
-    longesttime = 100
-    for i in range(5):
-        tmp = a[i] % 10
-        if tmp == 0:
-            tmp = 10
-        if tmp < longesttime:
-            longesttime = a[i] % 10
-    for i in range(5):
-        totaltime += math.ceil(a[i] / 10) * 10
-    print(totaltime - (10 - longesttime))
 
 
