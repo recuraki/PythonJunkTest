a958ff4268bffa260a9a376397075eeb770acd44
Akira Kanai <kanai@wide.ad.jp>
2019-07-06 14:53:33 +0900

    約数関数を追加

--- atcoder/_lib.py
+++ atcoder/_lib.py
@@ -6,6 +6,17 @@ def factorial_mod(n, mod):
     import math
     return math.factorial(n) % mod
 
+# 約数のリスト
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+    # divisors.sort()
+    return divisors
+
 # 組み合わせ
 def nCr(n, r):
     import math
